09d77219015eb5e0329cb7319e206e3916d1e51d
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-07-07 19:00:56 +0900

    add inmemory database

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,12 @@
 package graph
 
+import "github.com/physphys/gqlgen-todos/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+// in-memory database
+type Resolver struct {
+	todos []*model.Todo
+}
