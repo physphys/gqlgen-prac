0fabb6cc857d3201a5af0a432bd7606e6afe7c1c
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-07-07 19:01:15 +0900

    implement interface

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,17 +6,27 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/physphys/gqlgen-todos/graph/generated"
 	"github.com/physphys/gqlgen-todos/graph/model"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	rand.Seed(time.Now().UnixNano())
+
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", rand.Intn(100)),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
